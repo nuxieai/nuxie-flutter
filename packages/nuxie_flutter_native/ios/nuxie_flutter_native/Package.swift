3fbb41328900a97e9463529cc933403a3388e8a2
Levi McCallum <levi@levimccallum.com>
2026-09-18 16:23:28 -0700

    chore: adopt released video runtime SDK pins

--- packages/nuxie_flutter_native/ios/nuxie_flutter_native/Package.swift
+++ packages/nuxie_flutter_native/ios/nuxie_flutter_native/Package.swift
@@ -4,7 +4,7 @@ import Foundation
 
 let nativeDependency: Package.Dependency = ProcessInfo.processInfo.environment["NUXIE_IOS_SDK_PATH"].map {
   .package(name: "nuxie-ios", path: $0)
-} ?? .package(url: "https://github.com/nuxieai/nuxie-ios.git", revision: "858321e2e57cc62b6cb978a97834ad068f748c02")
+} ?? .package(url: "https://github.com/nuxieai/nuxie-ios.git", revision: "95d76d41eb4cc945cb57e5c1bcd8333ed15d55cc")
 
 let package = Package(
   name: "nuxie_flutter_native",
