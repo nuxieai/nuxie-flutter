4be0c840f91403eec949c9acd1b9e4f821cf5e82
Levi McCallum <levi@levimccallum.com>
2026-09-18 17:03:35 -0700

    fix: adopt lease-safe iOS video cache guard

--- packages/nuxie_flutter_native/ios/nuxie_flutter_native/Package.swift
+++ packages/nuxie_flutter_native/ios/nuxie_flutter_native/Package.swift
@@ -4,7 +4,7 @@ import Foundation
 
 let nativeDependency: Package.Dependency = ProcessInfo.processInfo.environment["NUXIE_IOS_SDK_PATH"].map {
   .package(name: "nuxie-ios", path: $0)
-} ?? .package(url: "https://github.com/nuxieai/nuxie-ios.git", revision: "95d76d41eb4cc945cb57e5c1bcd8333ed15d55cc")
+} ?? .package(url: "https://github.com/nuxieai/nuxie-ios.git", revision: "48fa51d6591f61d437620abfa06eb7fcb1a64564")
 
 let package = Package(
   name: "nuxie_flutter_native",
